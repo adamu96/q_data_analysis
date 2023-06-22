5e081dd98c03c615597d9bec541fce2a14bde485
Adam <adamu_1996@hotmail.com>
2023-06-22 13:34:22 +0100

    Resolved data pipeline issues (dtypes)

--- backups/qardio_db_22_06_2023.sql
+++ backups/qardio_db_22_06_2023.sql
@@ -3,7 +3,8 @@
 -- Host: localhost    Database: qardio
 -- ------------------------------------------------------
 -- Server version	8.0.32
-
+CREATE DATABASE qardio;
+USE qardio;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
